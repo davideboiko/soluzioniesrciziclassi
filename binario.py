60301f206e31ac2ad70463d9af4b6362458015c0
davideboiko <98486339+davideboiko@users.noreply.github.com>
2025-06-10 11:16:38 +0200

    Add files via upload

---
+++ binario.py
@@ -0,0 +1,12 @@
+def F1(t, F1):
+    r = []
+    for c in t:
+        if c.isalpha():
+            o = ord('A') if c.isupper() else ord('a')
+            r.append(chr((ord(c) - o + F1) % 26 + o))
+        else:
+            r.append(c)
+    return ''.join(r)
+print(F1("ciao come stai?", 5))
+
+
